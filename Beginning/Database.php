8928bd703ffb33546d401ace4763852a964ddc81
ssurerus <saulosurerus@hotmail.com>
2023-06-26 18:14:56 -0300

    refactoring code of mysql connection

--- Beginning/Database.php
+++ Beginning/Database.php
@@ -9,24 +9,22 @@ class Database
         // $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};username=root;charset={$config['charset']}";
         //simpler version
         //http_build_query first receives the data you gonna work with, if you want a prefix for ir {yourdomain ex} and a separator 
-        $dsn = 'mysql:' . http_build_query($config, '', ';'); 
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
 
-        $this->connection = new PDO($dsn, $username, $password, [          //dsn = Data Source Name
+        $this->connection = new PDO($dsn, $username, $password, [
+                //dsn = Data Source Name
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]); 
+        ]);
 
     }
-    public function query($query)
+    public function query($query, $parameters = [])
     {
-
         //Mysql Connection
-
-
         $statement = $this->connection->prepare($query);
 
-        $statement->execute();
-
-        return $statement->fetchAll(PDO::FETCH_ASSOC);
+        // The execute method will treat any content from our variable $id as a literal value, so even our user types something like ?id=1;drop table products it'll be interpretaded like a string and not as a command
+        $statement->execute($parameters);
+        return $statement; // Just return the statement is a good way to let us choose the fetch method we gonna use upward
     }
 
 }
--- Beginning/index.php
+++ Beginning/index.php
@@ -4,10 +4,14 @@ require 'functions.php';
 require 'Database.php';
 
 $config = require 'config.php';
-
 $db = new Database($config['database']);
-$products = $db->query("select * from products");
 
+$id = $_GET['id'];
+// $query = "select * from products where id = {$id}"; //This inline solution would let our code able to suffer MySqlInjection
+$query = "select * from products where id = :id"; 
+
+$products = $db->query($query, [':id'=>$id])->fetch();
+dd($id);
 foreach ($products as $product) {
     echo "<li>{$product['product_title']}</li>";
     echo "<li>{$product['product_description']}</li>";
